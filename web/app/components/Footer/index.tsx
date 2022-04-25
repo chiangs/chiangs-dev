84e1af4367bff1670d6d77fa72e9fdc40637e3a6
Stephen Chiang <stephen.chiang@webstep.no>
2022-04-25 15:36:29 +0200

    Add external links to design and project management board

--- web/app/components/Footer/index.tsx
+++ web/app/components/Footer/index.tsx
@@ -18,6 +18,8 @@ export const Footer: React.FC = () => {
     const year = <span>&nbsp;{now.getFullYear()},&nbsp;</span>;
     const me = ME;
     const name = `${me.firstName} ${me.lastName}`;
+    const designFileUrl = `https://www.figma.com/file/d7dPcBdKZghY6jwQQWmHLo/Project?node-id=0%3A1`;
+    const projectTaskBoardUrl = `https://chiangs.notion.site/Project-Tasks-Board-a46f64bb554d4f6b8f3ee840dae6c144`;
     const ctaButtonProps: ButtonCTAProps = {
         name: `contact me`,
         type: `button`,
@@ -38,7 +40,31 @@ export const Footer: React.FC = () => {
                 <Avatar variant="contact" dimensions={55} />
                 <ButtonCTA {...ctaButtonProps}>{ctaButtonProps.name}</ButtonCTA>
             </div>
-            <ul className="list--footer--links list--nostyle">{footerLinks}</ul>
+            <ul className="list--footer--links list--nostyle">
+                {footerLinks}
+                <li className="footer--link">
+                    <a
+                        href={designFileUrl}
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="Figma design file"
+                        aria-label="click to open new tab to the Figma design file"
+                    >
+                        Figma file
+                    </a>
+                </li>
+                <li className="footer--link">
+                    <a
+                        href={projectTaskBoardUrl}
+                        target="_blank"
+                        rel="noreferrer noopener"
+                        title="LinkedIn via new tab"
+                        aria-label="click to open new tab to my LinkedIn profile"
+                    >
+                        Development status
+                    </a>
+                </li>
+            </ul>
             <div className="footer--misc">
                 <p>
                     {copyright}
